305826d30a9bba441095ddffa3a0e856b55de68f
Kcronus <98719414+Kcronus@users.noreply.github.com>
2024-09-22 22:49:04 -0500

    frontend work

--- frontEnd.jl
+++ frontEnd.jl
@@ -4,7 +4,11 @@ using Mousetrap
     window = Window(app)
 
     # Text Entry for future Rest API integration
-    entry_0 = Entry()
+    rest_API_Terminal = Entry()
+    set_text!(rest_API_Terminal, "Write here")
+    connect_signal_activate!(rest_API_Terminal) do self::Entry
+        println("Text is now: $(get_text(rest_API_Terminal))")
+    end
 
     # Simple output Box, currently for testing, also works to show the user their inputs are valid
     output_main = Label()
@@ -52,34 +56,25 @@ using Mousetrap
         present!(chooser)
     end
 
-
-    start_Date_Drop_Down = DropDown()
-    end_Date_Drop_Down = DropDown()
-    amount_Drop_Down = DropDown()
-    value_Drop_Down = DropDown()
-
     # TODO find better way to enter date/date range, cause I don't love that I am just hardcoding in every possible output for days
 
-    # Start Date Drop Down
-    push_back!(start_Date_Drop_Down, "Start Date") do self::DropDown
-        println("Start Date")
-        set_text!(output_main, "Start Date")
-    end
-    push_back!(start_Date_Drop_Down, "Day 1") do self::DropDown
-        println("Day 1")
-        set_text!(output_main, "Day 1")
-    end
-   
-    # End Date Drop Down
-    push_back!(end_Date_Drop_Down, "End Date") do self::DropDown
-        println("End Date")
-        set_text!(output_main, "End Date")
-    end
-    push_back!(end_Date_Drop_Down, "Day 2") do self::DropDown
-        println("Day 2")
-        set_text!(output_main, "Day 2")
+    # Start Date Text Entry
+    start_Date_Entry = Entry()
+    set_text!(start_Date_Entry, "Start Date (Format year-mm-dd)")
+    connect_signal_activate!(start_Date_Entry) do self::Entry
+        println("Start date is: $(get_text(start_Date_Entry))")
     end
 
+    # End Date Text Entry
+    end_Date_Entry = Entry()
+    set_text!(end_Date_Entry, "End Date") 
+    connect_signal_activate!(end_Date_Entry) do self::Entry
+        println("End date is: $(get_text(end_Date_Entry))")
+    end
+    
+    amount_Drop_Down = DropDown()
+    value_Drop_Down = DropDown()
+   
     # Amount Drop Down
     push_back!(amount_Drop_Down, "Amount") do self::DropDown
         println("Amount")
@@ -110,73 +105,71 @@ using Mousetrap
     push_back!(value_Drop_Down, "Day Max Temperature") do self::DropDown
         println("max_temp")
         set_text!(output_main, "max_temp")
-        # I want to do something like this so it might be easier to integrate with weahther_data_analysis(), but these lines cause issues with the dropdown signature and returns 'Nothing'
-        #check_for_weather_code = false
-        #check_for_temperature_max = true
-        #check_for_temperature_min = false
-        #check_for_precipitation_sum = false
-        #check_for_wind_speed_max = false
-        #check_for_precipitation_probability_max = false
-        
+        global check_for_weather_code = false
+        global check_for_temperature_max = true
+        global check_for_temperature_min = false
+        global check_for_precipitation_sum = false
+        global check_for_wind_speed_max = false
+        global check_for_precipitation_probability_max = false
+        return nothing
     end
     push_back!(value_Drop_Down, "Day Min Temperature") do self::DropDown
         println("min_temp")
         set_text!(output_main, "min_temp")
-        #check_for_weather_code = false
-        #check_for_temperature_max = false
-        #check_for_temperature_min = true
-        #check_for_precipitation_sum = false
-        #check_for_wind_speed_max = false
-        #check_for_precipitation_probability_max = false
+        global check_for_weather_code = false
+        global check_for_temperature_max = false
+        global check_for_temperature_min = true
+        global check_for_precipitation_sum = false
+        global check_for_wind_speed_max = false
+        global check_for_precipitation_probability_max = false
+        return nothing
     end
     push_back!(value_Drop_Down, "Wind") do self::DropDown
         println("wind")
         set_text!(output_main, "wind")
-        #check_for_weather_code = false
-        #check_for_temperature_max = false
-        #check_for_temperature_min = false
-        #check_for_precipitation_sum = false
-        #check_for_wind_speed_max = true
-        #check_for_precipitation_probability_max = false
+        global check_for_weather_code = false
+        global check_for_temperature_max = false
+        global check_for_temperature_min = false
+        global check_for_precipitation_sum = false
+        global check_for_wind_speed_max = true
+        global check_for_precipitation_probability_max = false
+        return nothing
     end
     push_back!(value_Drop_Down, "Code") do self::DropDown
         println("code")
         set_text!(output_main, "code")
-        #check_for_weather_code = true
-        #check_for_temperature_max = false
-        #check_for_temperature_min = false
-        #check_for_precipitation_sum = false
-        #check_for_wind_speed_max = false
-        #check_for_precipitation_probability_max = false
+        global check_for_weather_code = true
+        global check_for_temperature_max = false
+        global check_for_temperature_min = false
+        global check_for_precipitation_sum = false
+        global check_for_wind_speed_max = false
+        global check_for_precipitation_probability_max = false
+        return nothing
     end
     push_back!(value_Drop_Down, "Rain") do self::DropDown
         println("rain")
         set_text!(output_main, "rain")
-        #check_for_weather_code = false
-        #check_for_temperature_max = false
-        #check_for_temperature_min = false
-        #check_for_precipitation_sum = true
-        #check_for_wind_speed_max = false
-        #check_for_precipitation_probability_max = false
+        global check_for_weather_code = false
+        global check_for_temperature_max = false
+        global check_for_temperature_min = false
+        global check_for_precipitation_sum = true
+        global check_for_wind_speed_max = false
+        global check_for_precipitation_probability_max = false
+        return nothing
     end
     push_back!(value_Drop_Down, "Rain Chance") do self::DropDown
         println("rain_prob")
         set_text!(output_main, "rain_prob")
-        #check_for_weather_code = false
-        #check_for_temperature_max = false
-        #check_for_temperature_min = false
-        #check_for_precipitation_sum = false
-        #check_for_wind_speed_max = false
-        #check_for_precipitation_probability_max = true
-    end
-    
-    # Text entry for future REST API integration
-    set_text!(entry_0, "Write here")
-    connect_signal_activate!(entry_0) do self::Entry
-        println("Text is now: $(get_text(entry_0))")
+        global check_for_weather_code = false
+        global check_for_temperature_max = false
+        global check_for_temperature_min = false
+        global check_for_precipitation_sum = false
+        global check_for_wind_speed_max = false
+        global check_for_precipitation_probability_max = true
+        return nothing
     end
 
-    set_child!(window, vbox(entry_0, start_Date_Drop_Down, end_Date_Drop_Down, amount_Drop_Down, value_Drop_Down, hist_button, rest_API_Button, unknown_Purpose_Button, file_Explorer, output_main))
+    set_child!(window, vbox(rest_API_Terminal, start_Date_Entry, end_Date_Entry, amount_Drop_Down, value_Drop_Down, hist_button, rest_API_Button, unknown_Purpose_Button, file_Explorer, output_main))
     present!(window)
 end
 
