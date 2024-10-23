6647b94ef43b361ea666f3a5b2728accbc13f7e2
Robert <67173136+Shapsty@users.noreply.github.com>
2024-10-23 07:36:34 -0500

    added Bar Feature Test

--- test.jl
+++ test.jl
@@ -8,3 +8,17 @@ function create_histogram(values)
     # Now you can load the PNG if needed:
 
 end
+function create_bargraph(values)
+    gr()
+    how_many = []
+
+    for (i,j) in enumerate(values)
+        append!(how_many, i)
+    end
+
+    bar_plot = plot(bar(how_many, values))
+    xlabel!(bar_plot, "Days")
+    ylabel!(bar_plot, "Values")
+    title!(bar_plot, "Bar Plot")
+    png(bar_plot, "bar")
+end
